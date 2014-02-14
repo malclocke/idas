6081158ff8121de300afdb90a4866bd23486de9d
Malcolm Locke <malc@wholemeal.co.nz>
2014-02-14 23:36:55 +1300

    Use generic views and remove unused imports

--- spectra/urls.py
+++ spectra/urls.py
@@ -1,8 +1,20 @@
 from django.conf.urls import patterns, url
+from django.views.generic import DetailView, ListView
+from spectra import views
+from spectra.models import Spectrum
 
 urlpatterns = patterns('spectra.views',
-    url(r'^$', 'index', name='index'),
-    url(r'^(?P<spectrum_id>\d+)/$', 'show', name='show'),
+    url(r'^$',
+        ListView.as_view(
+            queryset=Spectrum.objects.all(),
+            context_object_name='spectra',
+            template_name='spectra/index.html'),
+        name='index'),
+    url(r'^(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Spectrum,
+            template_name='spectra/show.html'),
+        name='show'),
     url(r'^(?P<spectrum_id>\d+)/plot.png$', 'plot', name='plot'),
     url(r'^(?P<spectrum_id>\d+)/plot-small.png$', 'plot_small', name='plot_small'),
     url(r'^upload/$', 'upload', name='upload'),
--- spectra/views.py
+++ spectra/views.py
@@ -2,20 +2,10 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
+from django.views import generic
 from spectra.models import Spectrum
 from specreduce.specreduce import BessSpectra
 
-import datetime
-import random
-
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-from matplotlib.dates import DateFormatter
-
-
-def index(request):
-  spectra = Spectrum.objects.all()
-  return render_to_response('spectra/index.html', {'spectra': spectra})
 
 def upload(request):
   if request.method == 'POST':
@@ -26,10 +16,6 @@ def upload(request):
     context = {}
     return render_to_response('spectra/upload.html', context,
         context_instance=RequestContext(request))
-
-def show(request, spectrum_id):
-  spectrum = get_object_or_404(Spectrum, pk=spectrum_id) 
-  return render_to_response('spectra/show.html', {'spectrum': spectrum})
 
 def plot(request, spectrum_id):
     spectrum = get_object_or_404(Spectrum, pk=spectrum_id)
@@ -42,4 +28,3 @@ def plot_small(request, spectrum_id):
     response=HttpResponse(content_type='image/png')
     spectrum.print_png(response, figsize=(64,64), style='compact')
     return response
-
