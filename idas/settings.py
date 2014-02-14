e3d4cedfe0e5d4c702b1eb8cf07342205aab65c2
Malcolm Locke <malc@wholemeal.co.nz>
2014-02-14 23:14:43 +1300

    Move upload directory

--- idas/settings.py
+++ idas/settings.py
@@ -1,6 +1,8 @@
 # Django settings for idas project.
 import os
 
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -50,12 +52,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = PROJECT_PATH + '/uploads/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/uploads/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -66,8 +68,6 @@ STATIC_ROOT = ''
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
-
-PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 # Additional locations of static files
 STATICFILES_DIRS = (
