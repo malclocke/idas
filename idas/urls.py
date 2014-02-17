56b096c75297e767dfe8adc9a9d1a5260dbe6627
Malcolm Locke <malc@wholemeal.co.nz>
2014-02-17 23:09:51 +1300

    Add login page

--- idas/urls.py
+++ idas/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,6 +9,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'idas.views.home', name='home'),
     # url(r'^idas/', include('idas.foo.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {
+        'template_name': 'login.html'
+    }),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
